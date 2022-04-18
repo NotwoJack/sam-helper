d6ec4ca6d1d9044e61ce12805d6f023a2ee7d812
zhangyibo <notwojack@gmail.com>
2022-04-18 10:49:58 +0800

    update

--- src/main/java/Sentinel.java
+++ src/main/java/Sentinel.java
@@ -18,22 +18,36 @@ public class Sentinel {
     public static void main(String[] args) {
 
         //执行任务请求间隔时间最小值
-        int sleepMillisMin = 3000;
+        int sleepMillisMin = 10000;
         //执行任务请求间隔时间最大值
-        int sleepMillisMax = 5000;
+        int sleepMillisMax = 30000;
 
         //单轮轮询时请求异常（服务器高峰期限流策略）尝试次数
         int loopTryCount = 5;
 
+        //60次以后长时间等待10分钟左右
+        int longWaitCount = 0;
+
         Map<String, Map<String, Object>> init = Api.init();
 
+        boolean first = true;
         while (!Api.context.containsKey("end")) {
             try {
-                sleep(RandomUtil.randomInt(sleepMillisMin, sleepMillisMax));
+                if (first) {
+                    first = false;
+                } else {
+                    if (longWaitCount++ > 60) {
+                        longWaitCount = 0;
+                        System.out.println("执行60次循环后，休息10分钟左右再继续");
+                        sleep(RandomUtil.randomInt(50000, 70000));
+                    } else {
+                        sleep(RandomUtil.randomInt(sleepMillisMin, sleepMillisMax));
+                    }
+                }
 
                 List<GoodDto> goodDtos = null;
                 for (int i = 0; i < loopTryCount && goodDtos == null; i++) {
-                    sleep(RandomUtil.randomInt(100, 500));
+                    sleep(RandomUtil.randomInt(200, 1000));
                     goodDtos = Api.getCart(init.get("storeDetail"));
                 }
                 if (goodDtos == null) {
@@ -42,7 +56,7 @@ public class Sentinel {
 
                 Map<String, Object> capacityData = null;
                 for (int i = 0; i < loopTryCount && capacityData == null; i++) {
-                    sleep(RandomUtil.randomInt(100, 500));
+                    sleep(RandomUtil.randomInt(500, 1000));
                     capacityData = Api.getCapacityData(init.get("storeDetail"));
                 }
                 if (capacityData == null) {
