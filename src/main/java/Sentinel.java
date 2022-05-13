87a7c12509842b53157544eca2cfb4650c4f94d7
zhangyibo <598047103>
2022-05-13 13:56:00 +0800

    update

--- src/main/java/Sentinel.java
+++ src/main/java/Sentinel.java
@@ -26,13 +26,6 @@ public class Sentinel {
         //单轮轮询时请求异常（服务器高峰期限流策略）尝试次数
         int loopTryCount = 10;
 
-        //7点58分00秒时间触发 极速达
-        while ("1".equals(UserConfig.deliveryType) && !Api.timeTrigger("07:58:00")) {
-        }
-        //13点59分00秒时间触发 全城送
-        while ("2".equals(UserConfig.deliveryType) && !Api.timeTrigger("13:59:00")) {
-        }
-
         Api.init(UserConfig.deliveryType);
         AddressDto addressDto = null;
         while (addressDto == null){
@@ -43,6 +36,13 @@ public class Sentinel {
                 addressDto = addressDtoList.get(scanner.nextInt());
             }
             sleep(RandomUtil.randomInt(sleepMillisMin, sleepMillisMax));
+        }
+
+        //7点58分00秒时间触发 极速达
+        while ("1".equals(UserConfig.deliveryType) && !Api.timeTrigger("07:58:00")) {
+        }
+        //13点59分00秒时间触发 全城送
+        while ("2".equals(UserConfig.deliveryType) && !Api.timeTrigger("13:59:00")) {
         }
 
         List<CouponDto> couponList = Api.getCouponList();
